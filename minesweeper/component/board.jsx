b2bac94b1d693e9c1185dc5cf8f6ea221ea5471c
Daniel <danielwu116@gmail.com>
2021-04-28 15:45:45 -0400

    Start styling game

--- minesweeper/component/board.jsx
+++ minesweeper/component/board.jsx
@@ -12,7 +12,7 @@ class Board extends React.Component {
     return (
       <div>
         {this.props.board.grid.map((row, idx1) =>{
-          return( <div key={idx1}>
+          return( <div key={idx1} className="row">
             {row.map((tile,idx2) =>{
               return <Tile key={[idx1, idx2]} tile={tile} updateGame = {this.props.updateGame} />
             })}
