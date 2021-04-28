1689bd0908f97d58c07804a6ee302efed2eef1a0
ShenQi1996 <qisam1989@gmail.com>
2021-04-28 17:22:50 -0400

    last  part of the game

--- minesweeper/component/game.jsx
+++ minesweeper/component/game.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import * as Minesweeper from '../frontend/minesweeper.js';
 import Board from './board';
+import Tile from "./tile"
 console.log("Webpack is working");
 class Game extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      board: new Minesweeper.Board(10,8)
+      board: new Minesweeper.Board(10,17)
     }
 
     this.updateGame = this.updateGame.bind(this);
   }
 
-  updateGame() {
-
+  updateGame(tile, flagged) {
+    if(flagged){
+      tile.toggleFlag();
+    }else{
+      tile.explore();
+    }
+    this.setState({
+      board: this.state.board
+    })
   }
 
   render() {
--- minesweeper/component/tile.jsx
+++ minesweeper/component/tile.jsx
@@ -3,17 +3,15 @@ import React from "react"
 class Tile extends React.Component {
     constructor(props){
         super(props)
-
         this.handleClick = this.handleClick.bind(this);
-        this.state = {
-            explored: this.props.tile.explored,
-        }
     }
     
     handleClick(e) {
-        let newTile = this.props.tile;
-        newTile.explore();
-        this.setState({explored: newTile.explored});
+        if(e.altKey){
+            this.props.updateGame(this.props.tile, true)
+        }else{
+            this.props.updateGame(this.props.tile, false)
+        }
     }
 
     render() {
@@ -21,16 +19,21 @@ class Tile extends React.Component {
         if (tile.explored) {
             if (tile.bombed) {
                 return <div className="tile bombed">💣</div>
-            } else if (tile.flagged) {
-                return <div className="tile flagged">⚑</div>
             } else {
-                const count = tile.adjacentBombCount();
+                const count = tile.adjacentBombCount() || "";
                 return <div className="tile number">{count}</div>
             }
-        } else {
-            return <div className="tile unexplored" onClick={this.handleClick}></div>
+        }else if(tile.flagged) {
+                return <div  onClick={this.handleClick} className="tile flagged">⚑</div>
+        }else {
+            return <div className="tile unexplored" onClick={this.handleClick} ></div>
         }
     }
 }
 
 export default Tile
+
+
+// if (tile.flagged) {
+//     return <div className="tile flagged">⚑</div>
+// }
